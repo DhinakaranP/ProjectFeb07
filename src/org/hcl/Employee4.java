41b09d197cf8194b9326ab3a718714a777e17c78
DhinakaranP <sarathidhina@gmail.com>
2022-02-07 08:41:06 +0530

    second project done

--- src/org/hcl/Employee4.java
+++ src/org/hcl/Employee4.java
@@ -1,5 +1,14 @@
 package org.hcl;
 
 public class Employee4 {
+	public void add() {
+	
+System.out.println("add");
 
+	}
+
+	public static void main(String[] args) {
+		Employee4 o=new Employee4();
+		o.add();
+	}
 }
